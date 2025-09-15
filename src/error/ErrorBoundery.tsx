6c5b5ca669a46b3693f7422937ed49db862a49e5
Alizon Kingslen <kingslenalizon@icloud.com>
2025-09-15 09:54:25 -0400

    added a new error page for status code 500

--- src/error/ErrorBoundery.tsx
+++ src/error/ErrorBoundery.tsx
@@ -31,6 +31,30 @@ export default function ErrorBoundary() {
                     </Box>
                 </Container>
             );
-        }
+        } else if (error.status === 500)
+            return (
+                <Container className="h-screen w-full flex items-center justify-center">
+                    <Box className="border-1 border-gray-700 bg-gray-900 p-9 rounded-2xl ">
+                        <Text
+                            as="p"
+                            className="text-2xl font-bold text-center text-shadow-2xs text-shadow-amber-50"
+                        >
+                            {" "}
+                            Oops.. 500 Server Error!
+                        </Text>
+
+                        <Text
+                            as="p"
+                            className="font-light text-2xl text-center"
+                        >
+                            {" "}
+                            Just go back to the homepage
+                        </Text>
+                        <Link to={"/"} replace>
+                            <HomeIcon className="text-amber-500 h-10 animate-pulse flex items-center justify-center w-full" />
+                        </Link>
+                    </Box>
+                </Container>
+            );
     }
 }
